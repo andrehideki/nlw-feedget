cf3c146f83f3cded714b98aaa751bf0bb3c2254a
andrehideki <andre.hkiw.dev@gmail.com>
2022-05-09 22:42:05 -0300

    fix: start script

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 
+
+
 const app = express();
 const port = process.env.PORT || 3333;
 
+console.log(port);
 
 app.use(cors());
 app.use(express.json())
